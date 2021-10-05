1b2fe40be2bd8ed69e12043ae92cdef6397d7419
Björn Harrtell <bjorn@wololo.org>
2021-10-05 20:47:52 +0200

    Add flatgeobuf and silly import&use

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'eai-issue/config/environment';
 
+import { geojson as flatgeobuf } from 'flatgeobuf';
+
+console.log(flatgeobuf)
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
